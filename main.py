7358837a8fcde03ca9afbffbfcf312eb5bd440fa
sarrafsagar249-hub <sarrafsagar249@gmail.com>
2026-07-30 16:34:16 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from datetime import datetime
+import pytz
+import nepali_datetime
 
+# India Time
+india_timezone = pytz.timezone("Asia/Kolkata")
+india_now = datetime.now(india_timezone)
+
+# Nepal Time
+nepal_now = nepali_datetime.date.today()
+
+# India Date
+india_date = india_now.strftime("%A, %d %B %Y")
+
+# Nepal Date
+nepal_date = nepal_now.strftime("%K %d, %N %Y")
+
+# Day
+day = india_now.strftime("%A")
+
+print("Today's Date")
+
+print("---------------------------")
+
+print("🇳🇵 Nepal (BS)")
+print(nepal_date)
+
+print()
+
+print("🇮🇳 India (AD)")
+print(india_date)
+
+print()
+
+print("Day:", day)
